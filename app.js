7963782a1f05bf81fc50d3314a492fcbc791afe4
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-07-09 21:30:59 +0200

    change structure of html

---
+++ app.js
@@ -0,0 +1,15 @@
+function santasNav() {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll("nav a");
+
+  sections.forEach(section => {
+    section.addEventListener("mouseenter", function() {
+      const id = this.getAttribute("id");
+      const activeSection = document.querySelector(`a[href="#${id}"]`);
+      navLinks.forEach(nav => nav.classList.remove("active"));
+      activeSection.classList.add("active");
+    });
+  });
+}
+
+santasNav();
